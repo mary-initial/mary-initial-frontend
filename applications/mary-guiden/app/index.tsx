20fddce94b415a391cc441a7689f75b72e897aac
Ahmad-Hahmoud <ahhah@nuuday.dk>
2026-03-06 07:56:42 +0100

    added safearea

--- applications/mary-guiden/app/index.tsx
+++ applications/mary-guiden/app/index.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@apollo/client/react";
 import { graphql } from "@generated/gql";
 import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const GET_GREETING = graphql(`
   query GetGreeting {
@@ -16,15 +17,17 @@ export default function Marrys() {
   if (loading) return <Text>Loading...</Text>;
   if (error) return <Text>Error: {error.message}</Text>;
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Marry's Page</Text>
-      <Text style={{ marginBottom: 20 }}>{data?.greeting?.message}</Text>
-    </View>
+    <SafeAreaView>
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>Marry's Page</Text>
+        <Text style={{ marginBottom: 20 }}>{data?.greeting?.message}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
