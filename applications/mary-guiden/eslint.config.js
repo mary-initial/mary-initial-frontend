142e4314d2cd26f09197ba780c60d76f8aaec766
Mikkel Mørch <mmo@globeteam.com>
2026-03-12 13:10:40 +0100

    Update eslint config

--- applications/mary-guiden/eslint.config.js
+++ applications/mary-guiden/eslint.config.js
@@ -1,11 +1,15 @@
-const { defineConfig } = require("eslint/config");
+const { defineConfig, globalIgnores } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintConfigPrettier = require("eslint-config-prettier/flat");
+const pluginReact = require("eslint-plugin-react");
 
 module.exports = defineConfig([
-    expoConfig,
-    {
-        ignores: ["dist/*", "__generated__/*"],
-    },
-    eslintConfigPrettier,
+  expoConfig,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: { react: { version: "19" } },
+  },
+  pluginReact.configs.flat["jsx-runtime"],
+  globalIgnores(["../../lib", "dist/**/*", "src/__generated__/**/*"]),
+  eslintConfigPrettier,
 ]);
--- applications/mary-ui-app/eslint.config.js
+++ applications/mary-ui-app/eslint.config.js
@@ -1,11 +1,9 @@
-const { defineConfig } = require("eslint/config");
+const { defineConfig, globalIgnores } = require("eslint/config");
 const expoConfig = require("eslint-config-expo/flat");
 const eslintConfigPrettier = require("eslint-config-prettier/flat");
 
 module.exports = defineConfig([
-    expoConfig,
-    {
-        ignores: ["dist/*"],
-    },
-    eslintConfigPrettier,
+  expoConfig,
+  globalIgnores(["dist/**/*"]),
+  eslintConfigPrettier,
 ]);
